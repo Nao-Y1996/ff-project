8a9bcf8f7107b1cd147426ba45ec72c9b35d8280
Nao-Y1996 <60519909+Nao-Y1996@users.noreply.github.com>
2021-08-15 17:06:13 +0900

    usersとpostappを統合した

--- postapp/models.py
+++ postapp/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from django.utils import timezone
+from users.models import CustomUser
+
 
 # Create your models here.
 class Favorites(models.Model):
     favorite_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     talk_id = models.IntegerField(null=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.tali_id
@@ -15,8 +17,8 @@ class Favorites(models.Model):
 
 class Talks(models.Model):
     talk_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    to_user_id = models.ForeignKey(User, related_name="tid" ,on_delete=models.CASCADE)
-    from_user_id = models.ForeignKey(User, related_name="fid" ,on_delete=models.CASCADE)
+    to_user_id = models.ForeignKey(CustomUser, related_name="tid" ,on_delete=models.CASCADE)
+    from_user_id = models.ForeignKey(CustomUser, related_name="fid" ,on_delete=models.CASCADE)
     confirmed_by_to = models.BooleanField(default=False)
     confirmed_by_from = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
