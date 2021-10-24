a0476989b820d7bd5b90bf2075cfbd618edd25cc
Nao-Y1996 <60519909+Nao-Y1996@users.noreply.github.com>
2021-10-24 21:09:57 +0900

    favorite_check 引数の修正 talk期限の設定の変数を追加

--- postapp/views.py
+++ postapp/views.py
@@ -23,14 +23,15 @@ from django.db.models import Q
 import json
 from django.http.response import JsonResponse
 from datetime import date
-
+DAYS = 0
+HOURS=9
+MINUTES=2
 
 def mypage(request):
     return render(request, "postapp/mypage.html")
 
 
-def favorite_check(request, talk_id):
-    talk = Talks.objects.get(id=talk_id)
+def favorite_check(request, talk):
     try:
         Exist_favorites = talk.favorites_set.all().filter(user_id=request.user)[
             0] in request.user.favorites_set.all()
@@ -51,7 +52,7 @@ def talk_all(request):
     def is_active(talk):
         now = datetime.datetime.now()
         deadline = talk.created_at.replace(
-            tzinfo=None) + datetime.timedelta(days=0) + datetime.timedelta(hours=9) + datetime.timedelta(minutes=2)
+            tzinfo=None) + datetime.timedelta(days=DAYS) + datetime.timedelta(hours=HOURS) + datetime.timedelta(minutes=MINUTES)
         if deadline < now:
             is_talk_active = False
         else:
@@ -122,29 +123,16 @@ def talk_create(request):  # 新規トークフォーム
         return render(request, 'postapp/talk_create.html', {'form': form})
 
 
-
-
-
-def favorite_check(request, talk_id):
-    talk = Talks.objects.get(id=talk_id)
-    try:
-        Exist_favorites = talk.favorites_set.all().filter(user_id=request.user)[
-            0] in request.user.favorites_set.all()
-    except:
-        Exist_favorites = False
-
-    return Exist_favorites
-
-
 def talk_detail(request, talk_id):  # 既存トークフォーム
+    talk = Talks.objects.get(id=talk_id)
     if request.method == 'POST':
-        Exist_favorites = favorite_check(request, talk_id)
+        Exist_favorites = favorite_check(request, talk)
         form = MessageForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
 
-            talk = Talks.objects.get(id=talk_id)
+            # talk = Talks.objects.get(id=talk_id)
             talk.detail_opened = False
             talk.save()
 
@@ -161,33 +149,25 @@ def talk_detail(request, talk_id):  # 既存トークフォーム
             messages = Message.objects.filter(talk_id=talk_id).all
             return render(request, 'postapp/talk_detail.html', {'messages': messages, 'form': form, 'talk_id': talk_id, 'Exist_favorites': Exist_favorites})
     else:
-        talk = Talks.objects.get(id=talk_id)
         # 返信がない かつ 自分がトークの開始者である 時はトーク詳細に入れない(トーク一覧に戻される)
         if (not talk.exist_reply) & (talk.sending_user == request.user):
             return redirect("postapp:talk_all")
         else:
-            print('詳細にはいった！！')
             newest_message = Message.objects.filter(talk_id=talk_id).latest("created_at")#.order_by('-created_at')[0]
-            print("message : ",newest_message.content)
-            print(Message.objects.filter(talk_id=talk_id).count())
             if newest_message.sending_user != request.user:
-                print("in if before save",talk.detail_opened)
                 talk.detail_opened = True
                 talk.save()
-                print("in if before save",talk.detail_opened)
-            print("*"*40)
-            print("out of if ",talk.detail_opened)
 
             now = datetime.datetime.now()
-            talk = Talks.objects.get(id=talk_id)
-            created_at = talk.created_at.replace(
-                tzinfo=None) + datetime.timedelta(days=14) + datetime.timedelta(hours=9) + datetime.timedelta(minutes=2)
+            # talk = Talks.objects.get(id=talk_id)
+            dead_line = talk.created_at.replace(
+                tzinfo=None) + datetime.timedelta(days=DAYS) + datetime.timedelta(hours=HOURS) + datetime.timedelta(minutes=HOURS)
             #left_time = created_at - data
             # print(left_time)
 
             # トーク可能時間内かどうか判定し時間外なら事後公開情報画面へ推移
-            if created_at < now:
-                Exist_favorites = favorite_check(request, talk_id)
+            if dead_line < now:
+                Exist_favorites = favorite_check(request, talk)
                 sending_user = talk.sending_user
                 user_info = UserInfo.objects.get(user_id=sending_user)
 
@@ -198,7 +178,7 @@ def talk_detail(request, talk_id):  # 既存トークフォーム
                     "sending_user": request.user, "talk": talk_id, }
                 form = MessageForm(initial=initial_dict)
                 messages = Message.objects.filter(talk_id=talk_id).all
-                Exist_favorites = favorite_check(request, talk_id)
+                Exist_favorites = favorite_check(request, talk)
 
                 if not talk.exist_reply:
                     receiving_user = talk.receiving_user
